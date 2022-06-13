b647517e57f4ae46b7bc9bb1ab4a4b53ae7c954d
psyfer <psyferblock.io@gmail.com>
2022-06-14 02:33:56 +0300

    connected to products from database

--- itemsStructure.js
+++ itemsStructure.js
@@ -1,37 +1,39 @@
 let category_id=document.getElementById("search-bar-btn")
-let results_container=document.getElementById("results-container")
+
 
 // code that loads categories 
 
-
-  window.onload= function getCategory(e){
+getCategory()
+ function getCategory(){
         console.log("something")
-        e.preventDefault()
+        // e.preventDefault()
         axios({
             method:"get",
             url:"http://127.0.0.1:8000/api/get_products",
         }).then((response)=>{
         console.log("something")
-            console.log(response)
+            console.log(response.data["results"] )
             response.display
-            for (var i=0;i<response.data.itemslength;i++){
-                var id = response.data.items[i].id;
-                var name = response.data.items[i].name;
-                var category = response.data.items[i].category;
-                var price = response.data.items[i].price;
-                var image = response.data.items[i].image;
-
-                // results_container.innerHTML+=`
-                // <div class="item"id="${data.id}">
-                //             <div class="item-input">
-                //             <h3>name:${data.name}</h3>
-                //             <h3>category:${data.email}</h3>
-                //             <h3>price:${data.price}</h3>
-                //             </div>
-                //             <img src="${data.image}" alt="">
+            for (let i=0;i<response.data["results"].length;i++){
+                var id =response.data["results"][i].id
+                var name = response.data["results"][i].name;
+                var category = response.data["results"][i].category;
+                var price = response.data["results"][i].price;
+                var image =response.data["results"][i].image;
+                console.log(id,name,category,price,image)
+                let results_container=document.getElementById("results-container")
+                results_container.innerHTML+=`
+                <div class="item"id="${id}">
+                            <div class="item-input">
+                            <h3>name:${name}</h3>
+                            <h3>category:${category}</h3>
+                            <h3>price:${price}</h3>
+                            </div>
+                            <img src="${image}" alt="">
                             
-                //         </div>
-                // `
+                        </div>
+                `
+                
 
             }
         })
